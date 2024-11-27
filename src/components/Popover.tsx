921e84601b1f00bee87fa80f24b1d3a9a48550ec
pycanis <pycan@proton.me>
2024-11-27 22:24:28 +0100

    Custom controls + popovergit add .! working

---
+++ src/components/Popover.tsx
@@ -0,0 +1,63 @@
+import { ReactNode, useCallback, useEffect, useState } from "react";
+import { usePopper } from "react-popper";
+
+type Props = {
+  children: ReactNode;
+  triggerNode: ReactNode;
+};
+
+export const Popover = ({ triggerNode, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [triggerElement, setTriggerElement] = useState<HTMLDivElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(triggerElement, popperElement, { placement: "left-end" });
+
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (
+        popperElement &&
+        !popperElement.contains(event.target as Node) &&
+        triggerElement &&
+        !triggerElement.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    },
+    [popperElement, triggerElement]
+  );
+
+  const handleKeydown = useCallback((event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      setIsOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside, true);
+      document.addEventListener("keydown", handleKeydown);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside, true);
+      document.removeEventListener("keydown", handleKeydown);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside, true);
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  }, [isOpen, handleClickOutside, handleKeydown]);
+
+  return (
+    <>
+      <div ref={setTriggerElement} onClick={() => setIsOpen((prev) => !prev)}>
+        {triggerNode}
+      </div>
+
+      {isOpen && (
+        <div ref={setPopperElement} style={styles.popper} className="bg-red-200" {...attributes.popper}>
+          {children}
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/containers/Graph/GraphButton.tsx
@@ -0,0 +1,14 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+
+type Props = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+
+export const GraphButton = ({ children, ...rest }: Props) => {
+  return (
+    <button
+      className="flex items-center justify-center enabled:hover:bg-gray-200 last-of-type:border-none border-b border-gray-200 bg-white w-7 h-7 p-1"
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
