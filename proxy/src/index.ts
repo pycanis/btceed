03f9d7548a29c43d80a06ab4ff9eb859d373743e
pycanis <pycan@proton.me>
2024-12-11 15:03:58 +0100

    Move files, proxy

---
+++ proxy/src/index.ts
@@ -0,0 +1,70 @@
+import "dotenv/config";
+
+import tls from "tls";
+import WebSocket from "ws";
+
+const { ELECTRUM_HOST, ELECTRUM_PORT } = process.env;
+
+const WS_PORT = 80;
+
+const wss = new WebSocket.Server({ port: WS_PORT });
+
+wss.on("connection", (ws) => {
+  console.log("New WebSocket connection established.");
+
+  const tcpSocket = tls.connect({ host: ELECTRUM_HOST, port: Number(ELECTRUM_PORT) }, () => {
+    console.log(`Connected to ssl://${ELECTRUM_HOST}:${ELECTRUM_PORT}`);
+  });
+
+  ws.on("message", (message) => {
+    const stringifiedMessage = message.toString();
+
+    console.log("Received from WebSocket:", stringifiedMessage);
+
+    tcpSocket.write(stringifiedMessage + "\n");
+  });
+
+  tcpSocket.on("data", (data) => {
+    const stringifiedData = data.toString();
+
+    console.log("Received from TCP:", stringifiedData);
+
+    const split = stringifiedData.replace(/\r\n/g, "\n").split("\n").filter(Boolean);
+
+    console.log(split);
+
+    for (const chunk of split) {
+      ws.send(chunk);
+    }
+  });
+
+  tcpSocket.on("end", () => {
+    console.log("TCP server connection ended");
+
+    ws.close();
+  });
+
+  tcpSocket.on("error", (err) => {
+    console.error("TCP socket error:", err);
+  });
+
+  tcpSocket.on("close", () => {
+    console.log("TCP server connection closed");
+
+    ws.close();
+  });
+
+  ws.on("close", () => {
+    console.log("WebSocket connection closed");
+
+    tcpSocket.end();
+  });
+
+  ws.on("error", (err) => {
+    console.error("WebSocket error:", err);
+
+    tcpSocket.end();
+  });
+});
+
+console.log(`WebSocket server running on ws://localhost:${WS_PORT}`);
